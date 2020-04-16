90481a3abeabffb1e066c8455bcd08ee316f8b11
ikReza <59397779+ikReza@users.noreply.github.com>
2020-04-16 17:55:06 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,14 +11,3 @@ function App() {
 }
 
 export default App;
-
-/* #1 */
-//[hualla-hullaji la-ilaha-illa-hu((a))] [a'limul-goibi wash-shahada((ti))] [huar-rohmanur rohim]
-
-/* #2 */
-//[hualla-hullaji la-ilaha-illa-hua (al-malikul kuddu'chuch)-(salamul) (mu'minul) (muhaiminul) (azizul) (jabbarul)
-//mutakabbir((u))] [subhanallahi ammma-yusrikun]
-
-/* #3 */
-//[hualla-hul kholikul bariul muchaouee'ru-lahul asma~ul-husna] [yuchabbihu-lahu ma~fissama-wa~ti wal-ard((i))]
-//[wa-hual azizul hakim]
